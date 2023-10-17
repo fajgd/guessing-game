a418437d5acded8828894f2ddb22e9fdb0cddb6f
Maksim Aliantsevish <132204061+fajgd@users.noreply.github.com>
2023-10-17 12:44:07 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const GuessingGame = require('./src/guessing-game.js');
 
 window.game = new GuessingGame();
+class GuessingGame {
+  constructor() {
+    this.min = null;
+    this.max = null;
+  }
+
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+
+  guess() {
+    return Math.floor((this.min + this.max) / 2);
+  }
+
+  lower() {
+    this.max = this.guess() - 1;
+  }
+
+  greater() {
+    this.min = this.guess() + 1;
+  }
+}
